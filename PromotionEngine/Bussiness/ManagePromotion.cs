83f459cbe25b345ad498c94bcc5015efeed65ad9
Sidd <siddheshmalvankar@gmail.com>
2021-10-05 14:27:23 +0530

    Add files via upload

---
+++ PromotionEngine/Bussiness/ManagePromotion.cs
@@ -0,0 +1,36 @@
+﻿using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PromotionEngine.Bussiness
+{
+    public class ManagePromotion 
+    {
+        private Dictionary<SKU, IPromotionStrategy> _strategies;
+        public ManagePromotion(Dictionary<SKU, IPromotionStrategy> strategies)
+        {
+            _strategies = strategies;
+        }
+        public decimal Compute(IList<SKU> skus)
+        {
+            decimal sumTotal = 0;
+            foreach (var s in _strategies)
+            {
+                var sku = skus.Where(p => p.SkuId == s.Key.SkuId).ToList();
+                if (sku.Count > 0)
+                {
+                    var val = s.Value.GetBestPrice(sku.First().Quantity);
+                    sumTotal = sumTotal + val;
+                    Console.WriteLine($"Best price for SKU:{sku.First().SkuId} and Quantity: {sku.First().Quantity}  = {val}");
+                }
+                   
+                 
+            }
+
+            return sumTotal;
+        }
+
+    }
+}
